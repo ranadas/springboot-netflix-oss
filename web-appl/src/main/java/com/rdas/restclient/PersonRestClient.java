9fce9663bc6fb2b08677232726dc618c1f9b7d46
Rana Das <rana.pratap.das@gmail.com>
2018-05-06 08:38:36 +0100

    clean

--- web-appl/src/main/java/com/rdas/restclient/PersonRestClient.java
+++ web-appl/src/main/java/com/rdas/restclient/PersonRestClient.java
@@ -1,15 +1,11 @@
 package com.rdas.restclient;
 
-//import org.springframework.cloud.netflix.feign.FeignClient;
-//import org.springframework.hateoas.Resources;
 import com.rdas.model.Person;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-
-//import com.orders.dto.Details;
 
 @FeignClient(
         name = "application.yml",
@@ -19,6 +15,4 @@ public interface PersonRestClient {
 
     @GetMapping("/persons")
     public List<Person> getPersons();
-//    Resources<Details> readDetails(@PathVariable("idProduct") long id);
-
 }
